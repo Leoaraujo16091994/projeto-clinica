3de2207f284eaa021d8d8016f2e1dba117f52b8b
leo <leo-tok@hotmail.com>
2022-06-26 01:36:19 -0300

    colocando o botao de perfil para sair ,tornando o menu lateral esquerdo do layout dinamico

--- app/Http/Controllers/PainelController.php
+++ app/Http/Controllers/PainelController.php
@@ -17,59 +17,75 @@ class PainelController extends Controller
         public function index ()
         {    
                 $pacientesDoDia = DB::table('pacientes_do_dia as pacDia1')
-                ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
-                ->select('pacDia1.sala_do_dia', 
-                        (DB::raw('(
-                                SELECT `pac2`.`id` 
-                                        FROM `paciente` AS `pac2` 
-                                    INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
-                                                `pac2`.`id` = `pacDia2`.`paciente_pk`
-                                     WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) idDoPaciente')), 
-                        (DB::raw('MAX(pacDia1.updated_at) ultimoAtualizado')),
-                        (DB::raw('(
-                                SELECT `pac3`.`nome_completo` 
-                                        FROM `paciente` AS `pac3` 
-                                INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
-                                        `pac3`.`id` = `pacDia3`.`paciente_pk`
-                                WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) nome_completo'))
-                        )
-                ->where('pacDia1.chamado', '=', '2')
-                ->whereDate('pacDia1.created_at',today())
-                ->groupBy('pacDia1.sala_do_dia')
-                ->get();
+                                ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
+                                ->select('pacDia1.sala_do_dia', 
+                                        (DB::raw('
+                                                (
+                                                        SELECT `pac2`.`id` 
+                                                        FROM `paciente` AS `pac2` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
+                                                                `pac2`.`id` = `pacDia2`.`paciente_pk`
+                                                        WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                        AND date(`pacDia2`.`created_at`) = (SELECT CURDATE())
+                                                ) idDoPaciente')), 
+                                        (DB::raw('MAX(pacDia1.updated_at) ultimoAtualizado')),
+                                        (DB::raw('
+                                                (
+                                                        SELECT `pac3`.`nome_completo` 
+                                                        FROM `paciente` AS `pac3` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
+                                                                `pac3`.`id` = `pacDia3`.`paciente_pk`
+                                                        WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                        AND date(`pacDia3`.`created_at`) = (SELECT CURDATE())
+                                                ) nome_completo'))
+                                        )
+                                ->where('pacDia1.chamado', '=', '2')
+                                ->whereDate('pacDia1.created_at',today())
+                                ->groupBy('pacDia1.sala_do_dia')
+                                ->get();
+                
                 
                 
                 $ultimoPacienteChamado = DB::table('pacientes_do_dia as pacDia1')
                                 ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
                                 ->select('pacDia1.sala_do_dia', 
-                                        (DB::raw('(
-                                                SELECT `pac2`.`id` 
-                                                        FROM `paciente` AS `pac2` 
-                                                INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
-                                                                `pac2`.`id` = `pacDia2`.`paciente_pk`
-                                                WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) idDoPaciente')), 
+                                        (DB::raw('
+                                                        (
+                                                                SELECT `pac2`.`id` 
+                                                                FROM `paciente` AS `pac2` 
+                                                                INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
+                                                                        `pac2`.`id` = `pacDia2`.`paciente_pk`
+                                                                WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                                AND date(`pacDia2`.`created_at`) = (SELECT CURDATE())
+                                                        ) idDoPaciente')
+                                        ), 
                                         (DB::raw('MAX(pacDia1.updated_at) ultimoAtualizado')),
-                                        (DB::raw('(
-                                                SELECT `pac3`.`nome_completo` 
-                                                        FROM `paciente` AS `pac3` 
-                                                INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
-                                                        `pac3`.`id` = `pacDia3`.`paciente_pk`
-                                                WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) nome_completo'))
+                                        (DB::raw('
+                                                        (
+                                                                SELECT `pac3`.`nome_completo` 
+                                                                FROM `paciente` AS `pac3` 
+                                                                INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
+                                                                        `pac3`.`id` = `pacDia3`.`paciente_pk`
+                                                                WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                                AND date(`pacDia3`.`created_at`) = (SELECT CURDATE())
+                                                        ) nome_completo'
+                                                )
+                                        )
                                         )
                                 ->where('pacDia1.chamado', '=', '2')
                                 ->whereDate('pacDia1.created_at',today())
                                 ->groupBy('pacDia1.sala_do_dia')
-                        ->orderByDesc('ultimoAtualizado')
-                        ->limit(1)
-                        ->get();
-                        
+                                ->orderByDesc('ultimoAtualizado')
+                                ->limit(1)
+                                ->get();
+                                
                 
                 
                 $ultimoPacienteChamadoSalvo= DB::table('ultimo_paciente_chamado')
-                        ->select('ultimo_paciente_chamado.paciente_pk')
-                        ->orderByDesc('ultimo_paciente_chamado.updated_at')
-                        ->limit(1)
-                        ->get();
+                                                ->select('ultimo_paciente_chamado.paciente_pk')
+                                                ->orderByDesc('ultimo_paciente_chamado.updated_at')
+                                                ->limit(1)
+                                                ->get();
 
 
                 
@@ -107,20 +123,30 @@ class PainelController extends Controller
                 $pacientesDoDia = DB::table('pacientes_do_dia as pacDia1')
                         ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
                         ->select('pacDia1.sala_do_dia', 
-                                (DB::raw('(
-                                        SELECT `pac2`.`id` 
-                                                FROM `paciente` AS `pac2` 
-                                            INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
-                                                        `pac2`.`id` = `pacDia2`.`paciente_pk`
-                                             WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) idDoPaciente')), 
+                                (DB::raw('
+                                                (
+                                                        SELECT `pac2`.`id` 
+                                                                FROM `paciente` AS `pac2` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
+                                                                        `pac2`.`id` = `pacDia2`.`paciente_pk`
+                                                        WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                        AND date(`pacDia2`.`created_at`) = (SELECT CURDATE())
+                                                ) idDoPaciente'
+                                        )
+                                ), 
                                 (DB::raw('MAX(pacDia1.updated_at) ultimoAtualizado')),
-                                (DB::raw('(
-                                        SELECT `pac3`.`nome_completo` 
-                                                FROM `paciente` AS `pac3` 
-                                        INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
-                                                `pac3`.`id` = `pacDia3`.`paciente_pk`
-                                        WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) nome_completo'))
+                                (DB::raw('
+                                                (
+                                                        SELECT `pac3`.`nome_completo` 
+                                                                FROM `paciente` AS `pac3` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
+                                                                `pac3`.`id` = `pacDia3`.`paciente_pk`
+                                                        WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`)
+                                                        AND date(`pacDia3`.`created_at`) = (SELECT CURDATE())
+                                                ) nome_completo'
+                                        )
                                 )
+                        )
                         ->where('pacDia1.chamado', '=', '2')
                         ->whereDate('pacDia1.created_at',today())
                         ->groupBy('pacDia1.sala_do_dia')
@@ -130,20 +156,30 @@ class PainelController extends Controller
                 $ultimoPacienteChamado = DB::table('pacientes_do_dia as pacDia1')
                                 ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
                                 ->select('pacDia1.sala_do_dia', 
-                                        (DB::raw('(
-                                                SELECT `pac2`.`id` 
-                                                        FROM `paciente` AS `pac2` 
-                                                INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
-                                                                `pac2`.`id` = `pacDia2`.`paciente_pk`
-                                                WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) idDoPaciente')), 
+                                        (DB::raw('
+                                                (
+                                                        SELECT `pac2`.`id` 
+                                                                FROM `paciente` AS `pac2` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia2` ON 
+                                                                        `pac2`.`id` = `pacDia2`.`paciente_pk`
+                                                        WHERE `pacDia2`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                        AND date(`pacDia2`.`created_at`) = (SELECT CURDATE())
+                                                ) idDoPaciente'
+                                                )
+                                        ), 
                                         (DB::raw('MAX(pacDia1.updated_at) ultimoAtualizado')),
-                                        (DB::raw('(
-                                                SELECT `pac3`.`nome_completo` 
-                                                        FROM `paciente` AS `pac3` 
-                                                INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
-                                                        `pac3`.`id` = `pacDia3`.`paciente_pk`
-                                                WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) ) nome_completo'))
+                                        (DB::raw('
+                                                (
+                                                        SELECT `pac3`.`nome_completo` 
+                                                                FROM `paciente` AS `pac3` 
+                                                        INNER JOIN `pacientes_do_dia` as `pacDia3` ON 
+                                                                `pac3`.`id` = `pacDia3`.`paciente_pk`
+                                                        WHERE `pacDia3`.`updated_at` = MAX(`pacDia1`.`updated_at`) 
+                                                        AND date(`pacDia3`.`created_at`) = (SELECT CURDATE())
+                                                ) nome_completo'
+                                                )
                                         )
+                                )
                                 ->where('pacDia1.chamado', '=', '2')
                                 ->whereDate('pacDia1.created_at',today())
                                 ->groupBy('pacDia1.sala_do_dia')
@@ -192,9 +228,8 @@ class PainelController extends Controller
         }  
 
         
-
         function chamarNovamente($id){
-             
+        /*     
                 
                $pacientesDoDia = DB::table('pacientes_do_dia as pacDia1')
                         ->join('paciente as pac1', 'pacDia1.paciente_pk', '=', 'pac1.id')
@@ -228,6 +263,6 @@ class PainelController extends Controller
                         ->get();
 
                 return view('/painel.novoPainelTelaCheia',['pacientesDoDia' => $pacientesDoDia],['ultimoPacienteChamado' => $ultimoPacienteChamado]);             
-           
+          */ 
         }
  }
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,81 +24,100 @@
   </head>
 
 
+
   <body class="nav-md">
     <div class="container body" style="max-width: 100%">
       <div class="main_container">
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="principal" class="site_title"><i class="fa fa-home"></i> <span> Clínica Prontorim</span></a>
+            <a href="principal" class="site_title"><i class="fa fa-home"></i> <span> Clínica Prontorim</span></a>
             </div>
 
-            <!-- NOME DO USUARIO LADO ESQUERDO -->
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
             <div class="profile clearfix">
+ <!--
               <div class="profile_info">
-                <span style="width:100%;color: #E7E7E7;"> <h2>Bem Vindo,
-               {{ Auth::user()->name }}</h2>
-                </span>
+                <span>Bem Vindo,</span>
+                <h2>{{ Auth::user()->name }}</h2>
               </div>
-            </div> 
+-->
+            </div>
     
-            <!-- MENUS DO LADO ESQUERDO -->
+ 
+            <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-              <!--  <h3>Geral</h3>-->
                 <ul class="nav side-menu">
-                  
-                 <li><a><i class="fa fa-edit"></i> Paciente <span class="fa fa-chevron-down"></span></a>
+                    <li><a><i class="fa fa-edit"></i> Paciente <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="/paciente">Pacientes</a></li>
-                 <!--      <li><a href="/chegada/create">Informar Chegada </a></li>
-                      <li><a href="/pacientes">Consultar Paciente </a></li>
-                    -->
+                    <li><a href="/paciente">Pacientes</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-desktop"></i> Painel <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-   <!--                   <li><a href="/chamarpainel/create"> Chamar Paciente </a></li>-->
-                      <li><a href="/painel"> Painel </a></li>
+                    <li><a href="/painel"> Painel </a></li>
                     </ul>
                   </li>
-                  <!--
-                  <li><a><i class="fa fa-table"></i> Relatorios <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="/relatorios/create">Relatorio  </a></li>
-                    </ul>
-                  </li>
--->
                 </ul>
               </div>
             </div>
-
-
+        
+            
+            <!-- /menu footer buttons -->
           </div>
         </div>
-        
+
+
+
+
         <!-- BOTOES CADASTRAR E SAIR  -->
         <div class="top_nav">
           <div class="nav_menu">
+               <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
             <ul class="nav" style="float:right;">
-              <!--<li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastrar') }}</a>
-              </li> -->
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                  {{ __('') }}<i class="fa fa-sign-out pull-right"></i> Sair 
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">    
-                  @csrf
-                </form>
-              </li>
+            <!-- <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <li><a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        {{ __('') }}<i class="fa fa-sign-out pull-right"></i> Sair </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">    
+                        @csrf
+                    </form>
+                  </li> -->
+                  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          {{ Auth::user()->name }}
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+          <li><a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                         Sair </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">    
+                        @csrf
+                    </form>
+                  </li>
+            
+          </ul>
+        </li>
             </ul>
            </div>
         </div>
-      
-      <br />
 
+
+        
       <div class="right_col" role="main">
             <div class="">
                 @yield('body')
@@ -120,16 +139,35 @@
   #titulopagina{
     font-size:20px;
   }
-  
   .nav {
-    display:  inline-flex;
+    display:  inline;
     flex-wrap: wrap;
     padding-left: 0;
     margin-bottom: 0;
     list-style: none;
   }
+  .navbar {
+    margin-bottom: 10;
+}
 
- 
+.profile_info {
+  padding:0;
+    width: 100%;
+    float: left;
+}
+
+.dropdown-menu > li > a:hover {
+    background-image: none;
+    background-color: #73879C;
+    color:white;
+}
+
+.top_nav .dropdown-menu li a {
+    float: right;
+    width: 50%;
+}
+    /*
+
   li {
     position:relative;
     width:15rem;
@@ -149,6 +187,6 @@
     text-align: right;
     width:3cm;
   }
-
+*/
 </style>
 
